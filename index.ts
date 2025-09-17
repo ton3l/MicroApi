2d69c5db5e42fd8c3424516d7dc69f1eb9b478a3
Elton <elton.felype.s.nascimento@gmail.com>
2025-09-17 09:44:51 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { parse } from "path";
+
+const names = ['Marçal', 'Daniel', 'Fábio'];
+
+console.log('server started\n');
+Bun.serve({
+    port: 3000,
+    routes: {
+        '/:id': (req: Request & { params: { id: string } }): Response => {
+            const parsedId = Number(req.params.id);
+
+            if (!isNaN(parsedId)) {
+                console.log(parsedId);
+                return new Response(names[parsedId]);
+            }
+
+            return new Response('Not Found', { status: 404 });
+        },
+    },
+});
