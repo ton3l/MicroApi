12fb1b52539c93634b612b2337d732b685c23738
Elton <elton.felype.s.nascimento@gmail.com>
2025-09-29 16:19:36 -0300

    feat: add express, student routes

--- index.ts
+++ index.ts
@@ -1,18 +1,55 @@
-const names = ['Marçal', 'Daniel', 'Fábio'];
+import express from 'express';
 
-console.log('server started\n');
-Bun.serve({
-    port: 3000,
-    routes: {
-        '/:id': (req: Request & { params: { id: string } }): Response => {
-            const parsedId = Number(req.params.id);
+const app = express();
+app.use(express.json());
+const port = 3000;
 
-            if (!isNaN(parsedId)) {
-                console.log(parsedId);
-                return new Response(names[parsedId]);
-            }
+const database = {
+    students: [] as Array<string>,
+};
 
-            return new Response('Not Found', { status: 404 });
-        },
-    },
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/students', (req, res) => {
+    res.status(200).send(database.students);
+});
+
+app.post('/students', (req, res) => {
+    console.log(req.body);
+    const newStudent = req.body.name;
+    database.students.push(newStudent);
+    res.status(201).send(newStudent);
+});
+
+app.put('/students/:name', (req, res) => {
+    const oldStudent = req.params.name;
+    const newStudent = req.body.name;
+
+    for (const [index, student] of database.students.entries()) {
+        if (student === oldStudent) {
+            database.students[index] = newStudent;
+            break;
+        }
+    }
+
+    res.status(200).send(newStudent);
+});
+
+app.delete('/students/:name', (req, res) => {
+    const delStudent = req.params.name;
+
+    for (const [index, student] of database.students.entries()) {
+        if (student === delStudent) {
+            database.students.splice(index, 1);
+            break;
+        }
+    }
+
+    res.status(200).send(0);
 });
